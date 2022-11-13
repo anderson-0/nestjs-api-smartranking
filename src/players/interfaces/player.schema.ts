12f61737323df33ceb21c4c8c20722b47276f840
Anderson Silva <60013836+anderson-0@users.noreply.github.com>
2022-11-13 16:23:46 -0300

    fix: removed phoneNumber as mongodb index

--- src/players/interfaces/player.schema.ts
+++ src/players/interfaces/player.schema.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 export const PlayerSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  phoneNumber: { type: String, required: true, unique: true },
+  phoneNumber: { type: String, required: true },
   ranking: { type: String },
   rankingPosition: { type: Number },
   urlPhoto: { type: String},
