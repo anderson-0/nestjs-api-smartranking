66eac6f6c309b872a0b732620b5aef3b96e73ea1
Anderson Silva <60013836+anderson-0@users.noreply.github.com>
2022-11-13 16:59:18 -0300

    Create create-category.dto.ts

---
+++ src/categories/dtos/create-category.dto.ts
@@ -0,0 +1,16 @@
+import { IsString, IsNotEmpty, IsArray, ArrayMinSize } from 'class-validator'
+import { IEvent } from '../interfaces/category.interface';
+
+export class CreateCategoryDto {
+  @IsString()
+  @IsNotEmpty()
+  readonly category: string;
+
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+
+  @IsArray()
+  @ArrayMinSize(1)
+  events: Array<IEvent>;
+}
