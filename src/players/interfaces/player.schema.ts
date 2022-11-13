5c6d56e878adcfbdbb633ba668a606878b7b32d2
Anderson Silva <60013836+anderson-0@users.noreply.github.com>
2022-11-13 14:01:04 -0300

    fix: remove required fields

--- src/players/interfaces/player.schema.ts
+++ src/players/interfaces/player.schema.ts
@@ -4,7 +4,7 @@ export const PlayerSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   phoneNumber: { type: String, required: true, unique: true },
-  ranking: { type: String, required: true },
-  rankingPosition: { type: Number, required: true },
-  urlPhoto: { type: String, required: true },
+  ranking: { type: String },
+  rankingPosition: { type: Number },
+  urlPhoto: { type: String},
 }, { timestamps: true, collection: 'players' });
