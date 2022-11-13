a6f0ddd074cad5ff0f7683ea027efb1267348835
Anderson Silva <60013836+anderson-0@users.noreply.github.com>
2022-11-13 17:30:55 -0300

    fix: added @Post and await

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Body, Controller } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { CreateCategoryDto } from './dtos/create-category.dto';
 import { ICategory } from './interfaces/category.interface';
@@ -7,11 +7,12 @@ import { ICategory } from './interfaces/category.interface';
 export class CategoriesController {
   constructor(private readonly categoriesService: CategoriesService) {}
 
+  @Post()
   async create(
     @Body() createCategoryDto: CreateCategoryDto,
   ): Promise<ICategory> {
     const { category } = createCategoryDto;
-    const categoryModel = this.categoriesService.findByCategory(category);
+    const categoryModel = await this.categoriesService.findByCategory(category);
 
     if (categoryModel) {
       throw new BadRequestException(`Category ${category} already exists`);
