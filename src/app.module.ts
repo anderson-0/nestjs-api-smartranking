372dc04f8dffc6058ceec617e3535df9b3d72868
Anderson Silva <60013836+anderson-0@users.noreply.github.com>
2022-11-13 16:37:16 -0300

    feature: generated categories module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PlayersModule } from './players/players.module';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGODB_URI),
-    PlayersModule
+    PlayersModule,
+    CategoriesModule
   ],
   controllers: [AppController],
   providers: [AppService],
