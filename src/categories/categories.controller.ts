78a8124cbb13f0b429619832d08c4c3199bdf8c3
Anderson Silva <60013836+anderson-0@users.noreply.github.com>
2022-11-13 16:59:41 -0300

    feature: added dummy create method to categories controller and service

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller } from '@nestjs/common';
+import { CategoriesService } from './categories.service';
+import { CreateCategoryDto } from './dtos/create-category.dto';
+import { ICategory } from './interfaces/category.interface';
 
 @Controller('categories')
-export class CategoriesController {}
+export class CategoriesController {
+  constructor(private readonly categoriesService: CategoriesService) {}
+
+  async create(
+    @Body() createCategoryDto: CreateCategoryDto,
+  ): Promise<ICategory> {
+    return await this.categoriesService.create(createCategoryDto);
+  }
+}
--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { CreateCategoryDto } from './dtos/create-category.dto';
+import { ICategory } from './interfaces/category.interface';
 
 @Injectable()
-export class CategoriesService {}
+export class CategoriesService {
+  constructor() {}
+
+  async create(createCategoryDto: CreateCategoryDto): Promise<ICategory> {
+    return null
+  }
+}
