33d4b09b9d109663c17c1d9148d3eebb721f91f8
Anderson Silva <60013836+anderson-0@users.noreply.github.com>
2022-11-14 19:25:37 -0300

    fix: removed moment-timezone

--- src/main.ts
+++ src/main.ts
@@ -7,16 +7,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new AllExceptionsFilter())
 
-  /*
-    We rewrite the toJSON method of the Date object to return the date in the format we want.
-    All Date objects will be affected by this change.
-  */
-  Date.prototype.toJSON = function(): any {
-    return momentTimezone(this)
-      .tz('America/Sao_Paulo')
-      .format('YYYY-MM-DD HH:mm:ss.SSS')
-  }
-
   await app.listen(8080);
 }
 bootstrap();
