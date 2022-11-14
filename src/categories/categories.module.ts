09602278eb3447d87ad046bc30e04b4c95c45938
Anderson Silva <60013836+anderson-0@users.noreply.github.com>
2022-11-14 15:58:23 -0300

    feature: export PlayersService to import it in CategoriesService

--- src/categories/categories.module.ts
+++ src/categories/categories.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { PlayersModule } from 'src/players/players.module';
+import { PlayersService } from 'src/players/players.service';
 import { CategoriesController } from './categories.controller';
 import { CategoriesService } from './categories.service';
 import { CategorySchema } from './interfaces/category.schema';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: 'Category', schema: CategorySchema }])],
+  imports: [
+    MongooseModule.forFeature([{ name: 'Category', schema: CategorySchema }]),
+    PlayersModule
+  ],
   controllers: [CategoriesController],
   providers: [CategoriesService]
 })
