e7ae36c65cbcae8451afae241f3c0998245f212d
Anderson Silva <60013836+anderson-0@users.noreply.github.com>
2022-11-14 15:58:46 -0300

    refactor: moved logic to service and added try catch to controller

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,7 +1,11 @@
-import { BadRequestException, Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Get, HttpCode, Param, Post, Put, UsePipes, ValidationPipe } from '@nestjs/common';
+import { PlayersParametersValidation } from 'src/players/pipes/players-parameters-validation.pipe';
+import { PlayersService } from 'src/players/players.service';
 import { CategoriesService } from './categories.service';
 import { CreateCategoryDto } from './dtos/create-category.dto';
+import { UpdateCategoryDto } from './dtos/update-category.dto';
 import { ICategory } from './interfaces/category.interface';
+import { CategoriesParametersValidation } from './pipes/categories-parameters-validation.pipe';
 
 @Controller('api/v1/categories')
 export class CategoriesController {
@@ -11,13 +15,13 @@ export class CategoriesController {
   async create(
     @Body() createCategoryDto: CreateCategoryDto,
   ): Promise<ICategory> {
-    const { category } = createCategoryDto;
-    const categoryModel = await this.categoriesService.findByCategory(category);
 
-    if (categoryModel) {
-      throw new BadRequestException(`Category ${category} already exists`);
+    try {
+      return await this.categoriesService.create(createCategoryDto);  
+    } catch (error) {
+      throw new BadRequestException(error.message);
     }
-    return this.categoriesService.create(createCategoryDto);
+    
   }
 
   @Get()
@@ -29,6 +33,33 @@ export class CategoriesController {
   async findById(@Param('_id') _id: string): Promise<ICategory> {
     try {
       return await this.categoriesService.findById(_id);
+    } catch (error) {
+      throw new BadRequestException(error.message);
+    }
+  }
+
+  @Put(':category')
+  @HttpCode(200)
+  @UsePipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true })) //removes unnecessary fields from the request body
+  async update(
+    @Body() updateCategoryDto: UpdateCategoryDto,
+    @Param('category', CategoriesParametersValidation) category: string): Promise<ICategory> {
+    try {
+      return await this.categoriesService.update(category, updateCategoryDto);
+    } catch (error) {
+      throw new BadRequestException(error.message);
+    }
+  }
+
+  @Post(':category/players/:playerId')
+  @HttpCode(200)
+  async addPlayerToCategory(
+    @Param('category', CategoriesParametersValidation) category: string,
+    @Param('playerId', PlayersParametersValidation) playerId: string,
+  ): Promise<ICategory> {
+    
+    try {
+      return await this.categoriesService.addPlayerToCategory(category, playerId);
     } catch (error) {
       throw new BadRequestException(error.message);
     }
--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,8 +1,10 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { PlayersService } from 'src/players/players.service';
 import { validate as uuidValidate } from 'uuid';
 import { CreateCategoryDto } from './dtos/create-category.dto';
+import { UpdateCategoryDto } from './dtos/update-category.dto';
 import { ICategory } from './interfaces/category.interface';
 
 @Injectable()
@@ -11,14 +13,56 @@ export class CategoriesService {
 
   constructor(
     @InjectModel('Category') private readonly categoryModel: Model<ICategory>,
+    private readonly playersService: PlayersService
   ) {}
 
   async create(createCategoryDto: CreateCategoryDto): Promise<ICategory> {
+    const { category } = createCategoryDto;
+    const foundCategory = await this.findByCategory(category);
+
+    if (foundCategory) {
+      throw new Error(`Category ${category} already exists`);
+    }
     const categoryModel = new this.categoryModel(createCategoryDto)
-    const category = await categoryModel.save()
+    const savedCategory = await categoryModel.save()
     
-    this.logger.log(`Saved Category: ${JSON.stringify(category)}`);
-    return category;
+    return savedCategory;
+  }
+
+  async update(category: string, updateCategoryDto: UpdateCategoryDto): Promise<ICategory> {
+    const categoryModel = await this.findByCategory(category);
+    if (!categoryModel) {
+      throw new Error(`Category ${category} not found`);
+    }
+    
+    const updatedCategory = await this.categoryModel.findOneAndUpdate({ category }, {
+      $set: updateCategoryDto
+    }, {new: true}).exec();
+
+    return updatedCategory;
+  }
+
+  async addPlayerToCategory(category: string, playerId: string): Promise<ICategory> {
+    const foundCategory = await this.findByCategory(category);
+    if (!foundCategory) {
+      throw new Error(`Category ${category} not found`);
+    }
+
+    const player = await this.playersService.findById(playerId);
+    if (!player) {
+      throw new Error(`Player ${playerId} not found`);
+    }
+
+    const playerAlreadyInCategory = foundCategory.players.find(player => player._id == playerId);
+    if (playerAlreadyInCategory) {
+      throw new Error(`Player ${playerId} already in category ${category}`);
+    }
+
+    foundCategory.players.push(player);
+    const updatedCategory = await this.categoryModel.findOneAndUpdate({ category }, {
+      $set: foundCategory
+      }, {new: true}).exec();
+    return updatedCategory
   }
 
   async find(): Promise<ICategory[]> {
