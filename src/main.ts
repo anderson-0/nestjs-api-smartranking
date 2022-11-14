3b3861ae094e7da0fa0c91dcfce83f6d0e5acb8d
Anderson Silva <60013836+anderson-0@users.noreply.github.com>
2022-11-14 18:15:20 -0300

    feature: change default Date.prototype.toJSON implementation

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import momentTimezone from 'moment-timezone';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new AllExceptionsFilter())
-  
+
+  /*
+    We rewrite the toJSON method of the Date object to return the date in the format we want.
+    All Date objects will be affected by this change.
+  */
+  Date.prototype.toJSON = function(): any {
+    return momentTimezone(this)
+      .tz('America/Sao_Paulo')
+      .format('YYYY-MM-DD HH:mm:ss.SSS')
+  }
+
   await app.listen(8080);
 }
 bootstrap();
